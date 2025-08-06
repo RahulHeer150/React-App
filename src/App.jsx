2757f1016a2a48b197a0f8ba1c85ce9673dcca5b
Rahul Heer <rahulheer344@gnail.com>
2025-08-06 23:00:56 +0530

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
 import './index.css'
 import Postcard from './Postcard'
+import Image from './assets/images.jpg'
+
 function App() {
   
   return (
     <>
     <div className='flex justify-center items-center min-h-screen bg-gray-900'>
       <Postcard 
+      img={Image}
       title="I K gujral Punjab technical University"
       description="this is a state government universiy established on 1997 at jalandhar Kapurthala highway."
+      />
+      <Postcard
+      img={Image}
+      title="Guru Nanak Dev Ji University"
+      description="this is a state government university established on 1969 at Amritsar."
       />
     </div>
     </>
--- src/Postcard.jsx
+++ src/Postcard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 
-const Postcard = ({title,description}) => {
+const Postcard = ({props}) => {
     const[likes,setlikes] = useState(0);
     const[liked,setliked] = useState(false);
     const handleLike = () => {
@@ -10,8 +10,13 @@ const Postcard = ({title,description}) => {
     };
   return (
     <div className='bg-white rounded-xl shadow-md p-6 w-80 text-center'>
-        <h2 className='text-xl font-bold text-grey-800 mb-2'>{title}</h2>
-        <p className='text-gray-600'>{description}</p>
+         <img
+        className="w-full h-48 object-cover"
+        src={props.imgPath}
+        alt="card-img"
+      />
+        <h2 className='text-xl font-bold text-grey-800 mb-2'>{props.title}</h2>
+        <p className='text-gray-600'>{props.description}</p>
         <button onclick={handleLike} className={`px-4 py-2 rounded-full text-white ${liked ? "bg-red-500":"bg-blue-500"}`}>
             {liked ? "unlike":"like"}({likes}) </button>
 
