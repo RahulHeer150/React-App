ddbc9dc3435c6ee62bc2b8b9f055e8dbbf97cc5d
Rahul Heer <rahulheer344@gnail.com>
2025-08-06 23:18:39 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import React from 'react'
 import { useState } from 'react'
 import './index.css'
 import Postcard from './Postcard'
-import Image from './assets/images.jpg'
+import Image from './assets/images.jpeg'
+import GNDU from './assets/gndu.jpg'
 
 function App() {
   
@@ -9,12 +11,12 @@ function App() {
     <>
     <div className='flex justify-center items-center min-h-screen bg-gray-900'>
       <Postcard 
-      img={Image}
+     imgPath={Image}
       title="I K gujral Punjab technical University"
       description="this is a state government universiy established on 1997 at jalandhar Kapurthala highway."
       />
       <Postcard
-      img={Image}
+     imgPath={GNDU}
       title="Guru Nanak Dev Ji University"
       description="this is a state government university established on 1969 at Amritsar."
       />
--- src/Postcard.jsx
+++ src/Postcard.jsx
@@ -1,27 +1,32 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from "react";
 
-const Postcard = ({props}) => {
-    const[likes,setlikes] = useState(0);
-    const[liked,setliked] = useState(false);
-    const handleLike = () => {
-        setliked(!liked);
-        setlikes((prev)=> (liked ? prev - 1 : prev + 1));
-    };
+const PostCard = (props) => {
+  const [likes, setLikes] = useState(0);
+  const [liked, setLiked] = useState(false);
+
+  const handleLike = () => {
+    setLiked(!liked);
+    setLikes((prev) => (liked ? prev - 1 : prev + 1));
+  };
+
   return (
-    <div className='bg-white rounded-xl shadow-md p-6 w-80 text-center'>
-         <img
-        className="w-full h-48 object-cover"
-        src={props.imgPath}
-        alt="card-img"
-      />
-        <h2 className='text-xl font-bold text-grey-800 mb-2'>{props.title}</h2>
-        <p className='text-gray-600'>{props.description}</p>
-        <button onclick={handleLike} className={`px-4 py-2 rounded-full text-white ${liked ? "bg-red-500":"bg-blue-500"}`}>
-            {liked ? "unlike":"like"}({likes}) </button>
-
+    <div className="bg-white rounded-xl shadow-md p-6 w-80 text-center m-5">
+        <img
+            src={props.imgPath}
+            alt="Postcard"
+            className="w-full h-40 object-cover rounded-t-xl mb-4"/>
+      <h2 className="text-xl font-bold text-gray-800 mb-2">{props.title}</h2>
+      <p className="text-gray-600 mb-4">{props.description}</p>
+      <button
+        onClick={handleLike}
+        className={`px-4 py-2 rounded-full text-white ${
+          liked ? "bg-red-500" : "bg-blue-500"
+        }`}
+      >
+        {liked ? "Unlike" : "Like"} ({likes})
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Postcard ;
+export default PostCard;
