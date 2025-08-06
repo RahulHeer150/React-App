96126b9ecfad2585e97ba8768eec4f4f53fe7527
Rahul Heer <rahulheer344@gnail.com>
2025-08-06 22:51:33 +0530

    tailwind with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import './index.css'
-
+import Postcard from './Postcard'
 function App() {
   
   return (
     <>
-    
+    <div className='flex justify-center items-center min-h-screen bg-gray-900'>
+      <Postcard 
+      title="I K gujral Punjab technical University"
+      description="this is a state government universiy established on 1997 at jalandhar Kapurthala highway."
+      />
+    </div>
     </>
   )
 }
---
+++ src/Postcard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Postcard = ({title,description}) => {
+    const[likes,setlikes] = useState(0);
+    const[liked,setliked] = useState(false);
+    const handleLike = () => {
+        setliked(!liked);
+        setlikes((prev)=> (liked ? prev - 1 : prev + 1));
+    };
+  return (
+    <div className='bg-white rounded-xl shadow-md p-6 w-80 text-center'>
+        <h2 className='text-xl font-bold text-grey-800 mb-2'>{title}</h2>
+        <p className='text-gray-600'>{description}</p>
+        <button onclick={handleLike} className={`px-4 py-2 rounded-full text-white ${liked ? "bg-red-500":"bg-blue-500"}`}>
+            {liked ? "unlike":"like"}({likes}) </button>
+
+    </div>
+  )
+}
+
+export default Postcard ;
