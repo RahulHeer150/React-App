623d8708f924f1edda3bef28ef44dd7854f0cdf7
Rahul Heer <rahulheer344@gmail.com>
2026-04-06 12:27:38 +0530

    new todo list component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,15 @@
-import TodoItem from "../TodoItem/TodoItem";
-import "./TodoList.css";
+import React from 'react'
+import TodoItem from "./TodoItem";
+//import "./TodoList.css";
 
 const TodoList = ({ todos, deleteTodo, toggleTodo }) => {
   return (
-    <div className="todo-list">
+    <div className="w-full mt-4 border-t border-gray-700 pt-4">
       {todos.length === 0 ? (
-        <p>No tasks yet 🚀</p>
+        <p>No tasks yet</p>
       ) : (
         todos.map((todo) => (
-          <TodoItem
+          <TodoItem 
             key={todo.id}
             todo={todo}
             deleteTodo={deleteTodo}
