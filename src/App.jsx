254e791c0d14cf70c9324e88777e881e7bbc91b2
Rahul Heer <rahulheer344@gmail.com>
2026-04-06 11:47:40 +0530

    new cde updations

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='min-h-screen flex items-center justify-center text-white bg-[#0f172a]'>
+      <Home />
+
     </div>
   )
 }
