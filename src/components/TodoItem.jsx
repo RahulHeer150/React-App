d89188aa81b530e258c2f004b73a4df847153bdd
Rahul Heer <rahulheer344@gmail.com>
2026-04-06 12:26:49 +0530

    new todo item component

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,14 +3,16 @@ import React from 'react'
 const TodoItem = ({todo, deleteTodo,toggleTodo}) => {
   return (
 
-    <div>
+    <div className='flex items-center justify-between mb-2'>
         <span 
         onClick={()=>toggleTodo(todo.id)}
         className={todo.completed ? 'line-through' : ''}
         >
             {todo.text}
         </span>
-        <button onClick={()=>deleteTodo(todo.id)}>Delete</button>
+        <button 
+        className='bg-red-500 px-3 py-1 rounded-lg text-white ml-4' 
+         onClick={()=>deleteTodo(todo.id)}>Delete</button>
 
     </div>
   )
