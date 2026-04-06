bf01d8a4728ec6b11801fdfc1e292b815ae677e9
Rahul Heer <rahulheer344@gmail.com>
2026-04-06 11:31:21 +0530

    new app code chnages

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
 import React from 'react'
-import { useState } from 'react'
-import './index.css'
-import Postcard from './Postcard'
-import Image from './assets/images.jpeg'
-import GNDU from './assets/gndu.jpg'
 
-function App() {
-  
+const App = () => {
   return (
-    <>
-    <div className='flex justify-center items-center min-h-screen bg-gray-900'>
-      <Postcard 
-     imgPath={Image}
-      title="I K gujral Punjab technical University kap"
-      description="this is a state government universiy established on 1997 at jalandhar Kapurthala highway."
-      />
-      <Postcard
-     imgPath={GNDU}
-      title="Guru Nanak Dev Ji University"
-      description="this is a state government university established on 1969 at Amritsar."
-      />
+    <div>
+      
     </div>
-    </>
   )
 }
 
