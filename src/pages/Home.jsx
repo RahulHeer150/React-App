458a04a989444e3167abd4fde1f5a6276309e875
Rahul Heer <rahulheer344@gmail.com>
2026-04-06 11:52:29 +0530

    new homepage component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div></div>
+    <div className='min-h-screen flex items-center justify-center text-white bg-[#0f172a]'  >
+        <h1>
+            To-Do App
+        </h1>
+        
+
+    </div>
   )
 }
 
