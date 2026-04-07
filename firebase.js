0677ed379c5b8da700c510c14f3f8763b111f829
Rahul Heer <rahulheer344@gmail.com>
2026-04-07 12:15:34 +0530

    starting firebase

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCzL3Jv4OF2Rsaa4pgqkZZZkt0Jp4l4r7Y",
+  authDomain: "qwon-c0c32.firebaseapp.com",
+  databaseURL: "https://qwon-c0c32-default-rtdb.firebaseio.com",
+  projectId: "qwon-c0c32",
+  storageBucket: "qwon-c0c32.firebasestorage.app",
+  messagingSenderId: "811552547458",
+  appId: "1:811552547458:web:80ee5157b72041f200ca9b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
